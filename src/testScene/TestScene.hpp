dd2ea52186b515dc682d88232569fe229e12997f
thu <syaseigaaru@gmail.com>
2021-04-28 05:07:37 +0900

    testScene

---
+++ src/testScene/TestScene.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "thuw/Scene/Scene.hpp"
+
+class TestScene final : public thuw::Scene::SceneInterface {
+public:
+    TestScene() {
+
+    }
+
+    void setup() {
+
+    }
+
+    void update() {
+
+    }
+};
